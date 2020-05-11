ebdba99b4d482e7fba12dbed667499b774118e48
Andy Chrzaszcz <andy9775@gmail.com>
2020-05-11 13:59:28 -0400

    implement event emitter in child to pass data to parent

--- src/app/mail.service.ts
+++ src/app/mail.service.ts
@@ -2,6 +2,13 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class MailService {
-  messages = ["You're now friends with John!", "You have a new tweet!"];
+  messages = [
+    { id: 1, value: "You're now friends with John!" },
+    { id: 2, value: "You have a new tweet!" },
+  ];
   constructor() {}
+
+  update(id, value) {
+    this.messages = this.messages.map((m) => (m.id == id ? { id, value } : m));
+  }
 }
