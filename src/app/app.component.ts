dcc9428be6af8ce9228cb3c0c4c0ae80a7621702
Andy Chrzaszcz <andy9775@gmail.com>
2020-05-11 13:12:03 -0400

    implement references to inputs
    \#myInput is used as a reference to various html elements
    we can then use myInput.value to get the value of an input
    or for other types of components we can get their attributes
    
    setting (click)="onClick" allows us to configure onClick handlers.
    The (event) syntax provides an easy way to hook into various types of
    events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
+  // can provide an inline template or a specific html template file
   // templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
   template: ` <div>
--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -4,15 +4,29 @@ import { Component, OnInit } from "@angular/core";
   selector: "app-simple-form",
   template: `
     <div>
-      <p>simple-form Works!</p>
-      <p>{{ hello }}</p>
+      <!-- access fields onthe class and display them -->
+      <div>{{ hello }}</div>
+      <!-- #myInput is ref to the input -->
+      <input #myInput type="text" />
+
+      <!-- (click) specifies the event handler -->
+      <!-- onClick(myInput.value) allows us to get the value of the input 'myInput' since myInput is the reference to <input> -->
+      <button (click)="onClick(myInput.value)">click me</button>
     </div>
   `,
   styles: [],
 })
 export class SimpleFormComponent implements OnInit {
-  hello = "hello world";
+  hello: string;
+
+  onClick(value: string) {
+    console.log(`got: ${value}`);
+  }
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // set the field when the component is initialized which then gets displayed
+    this.hello = "hello world!";
+  }
 }
